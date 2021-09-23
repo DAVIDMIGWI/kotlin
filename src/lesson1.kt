80a2569cf940ea2c9095d28ec2b51406ae2b9ace
DAVID <migwi81@gmail.com>
2021-09-23 12:03:25 +0300

    Initial commit

---
+++ src/lesson1.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println(5000)
+    println("hello")
+    // create a variable
+    // use val or var
+    var age = 26;
+    age = 24;
+    println(age)
+// val -  you cannot reasign the variable to another value
+// var - you can reassign the variable to another value
+}
---
+++ src/lesson1a.kt
@@ -0,0 +1,18 @@
+fun main(){
+    // datatypes - int, double, string, byte, char, long, float, short, boolean
+    val number1: Int= 45; // whole numbers from -2.2b to +2.2b
+    val number2: Double=45.0;
+    val number3: Int= 45;
+    val number4: Short= 3200; // whole number from -32k to 32k
+    var total:Double = 0.0;
+    total = number1+number2+number3+number4
+    println("your sum is "+total)
+    println("your sum is $total")
+
+
+}
+// to do.
+//1. create new kotlin file
+//2. bmi
+//3. simple intrerest(PTR/100)
+//4. Area of a circle(PIRsquared)
